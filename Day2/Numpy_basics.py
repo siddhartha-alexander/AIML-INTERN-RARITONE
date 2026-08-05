144021a1311e035090ab9daa6a25f7f000446d66
Damala Siddhartha Alexander <siddhudamala@gmail.com>
2026-08-05 12:28:11 +0530

    Fix Day 3 notebook files

--- Day2/Numpy_basics.py
+++ Day2/Numpy_basics.py
@@ -4,6 +4,7 @@ My_list=[1,2,3,4]
 My_list= My_list*2
 print(My_list)
 
+#Numpy array is way superior to List and it faster than List
 Numpy_list=np.array([1,2,3,4])
 Numpy_list=Numpy_list*2
 print(Numpy_list)
